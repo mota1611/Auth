16507073c84229f3fe7e17765f9227b51acc3069
Dm <52580787+mota1611@users.noreply.github.com>
2024-12-06 21:40:36 -0300

    Delete Server QRCodeProcess directory

--- Server QRCodeProcess/config/settings.py
+++
@@ -1,7 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-TOKENS_DIR = os.path.join(BASE_DIR, '..', 'storage', 'tokens')
-
-if not os.path.exists(TOKENS_DIR):
-    os.makedirs(TOKENS_DIR)
